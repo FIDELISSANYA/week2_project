3287bf24adb6c0152d9ade7b8f790465e508a84c
FIDELIS SANYA <fidelis.sanya@student.moringaschool.com>
2022-03-06 08:43:08 +0300

    incoperate akanName function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 let akanMaleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 let akanFemaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+function akanName() {
+    var dateOfBirth = document.getElementById("date").value;
+    var monthOfBirth = document.getElementById("month").value;
+    var yearOfBirth = document.getElementById("year").value;
+    var gender = document.getElementById("gender").value;
+}

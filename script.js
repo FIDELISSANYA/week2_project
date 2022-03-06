f7ffd4808598786bcf852f9f57782f3925017859
FIDELIS SANYA <fidelis.sanya@student.moringaschool.com>
2022-03-06 08:45:11 +0300

    Add day of the week calculation formula

--- script.js
+++ script.js
@@ -5,4 +5,9 @@ function akanName() {
     var monthOfBirth = document.getElementById("month").value;
     var yearOfBirth = document.getElementById("year").value;
     var gender = document.getElementById("gender").value;
+
+    let birthDay = new Date(parseInt(yearOfBirth),parseInt(monthOfBirth) , parseInt(dateOfBirth), 0, 0, 0, 0);
+    console.log("birthday "+birthDay);
+    var dateStr = birthDay.toDateString();
+    let birthDate = birthDay.getDay();
 }
